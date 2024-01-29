57a6a9dc893736bd9f3b7b64b08a8d92d6d8bc45
scavengerhuntavensis <157680005+scavengerhuntavensis@users.noreply.github.com>
2024-01-29 14:44:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+
+    let correct = document.getElementById("correct");
+    let mainContent = document.getElementById("mainContainer");
+
+
+    document.getElementById("math").addEventListener("input", (event) => {
+        let input = event.target.value;
+        checkAns(correct, mainContent, input);
+    });
+}
+
+
+const checkAns = (correct, mainContent, input) => {
+    if(md5(input) == "f3f58ee455ae41da2ad5de06bf55e8de" || md5(input) == "234782fd089cf4486e960fa0c570f590" || md5(input) == "5b11962dcbe85de55770d22d3e5fb85a" || md5(input) == "817857b0693710898411438234078b55" || md5(input) == "2d351990d450cc028c016c2943b07013" || md5(input) == "b631d0cfde68df7d5a68d8a4b60f45e2"){
+        mainContent.style.visibility = "hidden";
+        correct.innerHTML = "Yes you guessed it right!";
+        const fiveSeconds = setTimeout(() => {
+            mainContent.style.height = 0;
+            correct.innerHTML = "Guessing the prize might not have been easy, but found if you have, the location is one that could make you queasy, Now travel to the point where keepers of the gold keep the wealth, Where no magic works to steal- even stealth.";
+        }, 2000);
+    }
+}   
